f11983a75f93bf94602c3423659556030409d3d9
annabusaga <anna.busaga@gmail.com>
2024-07-01 22:26:59 +0300

    commit

--- js/task-1.js
+++ js/task-1.js
@@ -4,5 +4,4 @@ function slugify(title) {
 
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascaaaaaaaaaaaaaaaaaaasaaasdffghjkl;;;lkjhgfdsasdfghjklkjhgfdsdfghjklkjhgfdsdfghjklkjhgfdsasdfghjkjhgvfcdxszsdfghjklkjhgfdsasdfghjkl;lkjhgfdsertyuioiuytrewyuioijhgfdsdfghjklkjhresdfghjiklkjhgvcfghjklkjhgfghjklkjnbgvfghjkjhgfdfghjkl;';09liuytyvhj9iy7 r5v  nkgtmytynjkgl;lthjgkjmbkvmvhbtykfmjhritgnikgk
